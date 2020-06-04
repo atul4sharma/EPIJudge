c7e0d1f3842ee6b0fb424c90bb387f3fe36a66ed
Atul Sharma <atulsharma406@gmail.com>
2020-06-04 23:29:15 +0530

    Solved first set of questions

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,7 +1,23 @@
 #include "test_framework/generic_test.h"
+
+
+long long SwapBits(long long x, int i, int j) {
+  // TODO - you fill in here.
+  if( ((x >> i) & 1) != ((x >> j) & 1) )
+  {
+      auto bit_mask = (1L << i) | (1L << j);
+      x ^= bit_mask;
+  }
+  return x;
+}
+
 unsigned long long ReverseBits(unsigned long long x) {
   // TODO - you fill in here.
-  return 0;
+  for( auto i = 0, j = 63; i < 64/2; ++i, --j)
+  {
+      x = SwapBits(x, i, j);
+  }
+  return x;
 }
 
 int main(int argc, char* argv[]) {
