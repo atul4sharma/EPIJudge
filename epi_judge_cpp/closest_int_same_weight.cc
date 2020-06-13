1c6f4c4f7edb7d3a2d078fa61d4467b5d7070780
Atul Sharma <atulsharma406@gmail.com>
2020-06-13 21:37:08 +0530

    Solved closest_int_same_weight

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,7 +1,25 @@
 #include "test_framework/generic_test.h"
-unsigned long long ClosestIntSameBitCount(unsigned long long x) {
+
+long long SwapBits(long long x, int i, int j) {
   // TODO - you fill in here.
-  return 0;
+  if( ((x >> i) & 1) != ((x >> j) & 1) )
+  {
+      auto bit_mask = (1L << i) | (1L << j);
+      x ^= bit_mask;
+  }
+  return x;
+}
+
+unsigned long long ClosestIntSameBitCount(unsigned long long x) {
+  for(auto i = 0; i < 63; ++i)
+  {
+      if( ((x >> i) & 1) != ((x >> (i+1)) & 1) )
+      {
+          x = SwapBits(x, i, i+1);
+          break;
+      }
+  }
+  return x;
 }
 
 int main(int argc, char* argv[]) {
