540ecd658f92d87cd02d6ec6bc1a865d516e8f50
Atul Sharma <atulsharma406@gmail.com>
2020-06-17 23:55:22 +0530

    Add prime_seive

--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -4,8 +4,29 @@
 using std::vector;
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+    if( n < 2)
+        return {};
+
+    auto primes = std::vector<bool>(n+1, true);
+    primes[0] = false;
+    primes[1] = false;
+    auto num = int{2};
+    while( num <= n)
+    {
+        for( auto i = num + num; i <= n; i += num)
+        {
+            primes[i] = false;
+        }
+        num = std::distance(primes.begin()
+                           ,std::find(primes.begin()+ num + 1, primes.end(), true));
+    }
+
+    auto prime_numbers = std::vector<int>{};
+    for(auto i = 0; i <= n; ++i)
+        if(primes[i])
+            prime_numbers.emplace_back(i);
+  
+    return prime_numbers;
 }
 
 int main(int argc, char* argv[]) {
