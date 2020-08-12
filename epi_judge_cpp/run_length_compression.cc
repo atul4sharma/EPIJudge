23c78e399e1eb8a7f502a4a5e5c747e97e365f42
Atul Sharma <atulsharma406@gmail.com>
2020-08-12 23:49:06 +0530

    Add run_length_compression example

--- epi_judge_cpp/run_length_compression.cc
+++ epi_judge_cpp/run_length_compression.cc
@@ -3,13 +3,44 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 using std::string;
+
 string Decoding(const string &s) {
-  // TODO - you fill in here.
-  return "";
+    auto       result = std::string{};
+    auto       first  = s.begin();
+    auto const last   = s.end();
+    while( first != last )
+    {
+        auto const current_character = std::find_if(first
+                                                   ,last
+                                                   ,[] (unsigned char const c) {
+                                                      return std::isalpha(c);
+                                                   });
+        auto const count = std::stoi(s.substr(std::distance(s.begin(), first)
+                                             ,std::distance(first, current_character)));
+        result += std::string(count, *current_character);
+        first = std::next(current_character);
+    }
+    return result;
 }
+
 string Encoding(const string &s) {
-  // TODO - you fill in here.
-  return "";
+  auto       result = std::string{};
+  auto       first  = s.begin();
+  auto const last   = s.end();
+  while( first != last )
+  {
+      auto const character = *first;
+      auto next_char = std::find_if(first
+                                   ,last
+                                   ,[&character] (char const ch) {
+                                       return ch != character;
+                                   });
+      auto const count = std::distance(first, next_char);
+      result += std::to_string(count) + character;
+
+      first = next_char;
+  }
+  return result;
 }
 void RleTester(const string &encoded, const string &decoded) {
   if (Decoding(encoded) != decoded) {
