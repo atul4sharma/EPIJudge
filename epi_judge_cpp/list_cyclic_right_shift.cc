64cc182bebe38ab88c50f47bcdb05cbbacf5a077
Atul Sharma <atulsharma406@gmail.com>
2020-08-21 00:36:11 +0530

    Add list cyclic right shift example

--- epi_judge_cpp/list_cyclic_right_shift.cc
+++ epi_judge_cpp/list_cyclic_right_shift.cc
@@ -4,10 +4,43 @@
 #include "test_framework/generic_test.h"
 using std::shared_ptr;
 
+auto list_length(std::shared_ptr<ListNode<int>> l)
+    -> int
+{
+    auto count = 0;
+    while( l )
+    {
+        l = l->next;
+        ++count;
+    }
+    return count;
+}
+
 shared_ptr<ListNode<int>> CyclicallyRightShiftList(shared_ptr<ListNode<int>> L,
                                                    int k) {
-  // TODO - you fill in here.
-  return nullptr;
+    auto dummy_head = std::make_shared<ListNode<int>>();
+
+    auto const size = list_length(L);
+    if( size == 0 )
+        return nullptr;
+    auto       move_after = size - (k%size);
+
+    if( move_after == 0 || move_after == size)
+        return L;
+
+    auto iter = L;
+    while( --move_after > 0)
+        iter = iter->next;
+
+    dummy_head->next = iter->next;
+    iter->next = nullptr;
+
+    iter = dummy_head->next;
+    while(iter->next != nullptr)
+        iter = iter->next;
+
+    iter->next = L;
+  return dummy_head->next;
 }
 
 int main(int argc, char* argv[]) {
