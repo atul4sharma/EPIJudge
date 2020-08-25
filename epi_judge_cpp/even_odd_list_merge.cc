fb3ae3bc84a24fb37a27da24e192b9395062e04e
Atul Sharma <atulsharma406@gmail.com>
2020-08-26 01:37:59 +0530

    Add even_odd_list_merge example

--- epi_judge_cpp/even_odd_list_merge.cc
+++ epi_judge_cpp/even_odd_list_merge.cc
@@ -1,8 +1,25 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 shared_ptr<ListNode<int>> EvenOddMerge(const shared_ptr<ListNode<int>>& L) {
-  // TODO - you fill in here.
-  return nullptr;
+    auto even_start = L;
+    auto odd_start  = L;
+    if( L && L->next )
+        odd_start = L->next;
+    else
+        return even_start;
+
+    auto even_iter = even_start;
+    auto odd_iter = odd_start;
+    while(even_iter && even_iter->next && odd_iter && odd_iter->next)
+    {
+        even_iter->next       = odd_iter->next;
+        odd_iter->next        = odd_iter->next->next;
+        even_iter->next->next = odd_start;
+
+        even_iter = even_iter->next;
+        odd_iter  = odd_iter->next;
+    }
+    return even_start;
 }
 
 int main(int argc, char* argv[]) {
