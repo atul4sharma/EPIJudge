c271f8c8232c37751d22603d6faecdd103daf8f7
Atul Sharma <atulsharma406@gmail.com>
2020-08-31 23:15:25 +0530

    Add pascal triangle

--- epi_judge_cpp/pascal_triangle.cc
+++ epi_judge_cpp/pascal_triangle.cc
@@ -2,9 +2,33 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
+auto generate_row(std::vector<std::vector<int>> const & till_now
+                 ,int                           const   nth_row)
+    -> std::vector<int>
+{
+    if( nth_row == 1 )
+        return {1};
+    if( nth_row == 2 )
+        return {1,1};
+
+    auto row = std::vector<int>{1};
+    for( auto i = 0; i < nth_row - 2; ++i)
+    {
+        auto & n_minus_one_row = till_now[nth_row -2];
+        row.emplace_back( n_minus_one_row[i] + n_minus_one_row[i+1]);
+    }
+    row.emplace_back(1);
+    return row;
+}
+
 vector<vector<int>> GeneratePascalTriangle(int num_rows) {
-  // TODO - you fill in here.
-  return {};
+    auto result = std::vector<std::vector<int>>{};
+    for(auto i = 1; i <= num_rows; ++i)
+    {
+        result.emplace_back(generate_row(result, i));
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
