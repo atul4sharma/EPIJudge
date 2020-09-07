205fef14bbb7fb8a83c771bc4f2bd661467c33e3
Atul Sharma <atulsharma406@gmail.com>
2020-09-07 23:12:42 +0530

    Add tree_balanced example

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,40 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+
+struct height_and_balance
+{
+    bool const is_balanced;
+    int  const height;
+
+    height_and_balance(bool b, int h)
+        : is_balanced{std::move(b)}
+        , height{std::move(h)}
+    {}
+};
+
+
+auto is_balanced_impl(std::unique_ptr<BinaryTreeNode<int>> const & tree) 
+    -> height_and_balance
+{
+    if( tree == nullptr)
+        return {true, -1};
+
+    auto const left_height  = is_balanced_impl(tree->left);
+    if( !left_height.is_balanced )
+        return {false, 0};
+
+    auto const right_height = is_balanced_impl(tree->right);
+    if( !right_height.is_balanced )
+        return {false, 0};
+
+    bool const is_balanced = std::abs(left_height.height - right_height.height) <= 1;
+    int  const height      = std::max(left_height.height, right_height.height) + 1;
+
+    return {is_balanced, height};
+}
+
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+  return is_balanced_impl(tree).is_balanced;
 }
 
 int main(int argc, char* argv[]) {
