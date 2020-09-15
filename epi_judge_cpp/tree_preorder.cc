4d70441fbc2af81b13ccfaffb0febca7b073aae5
Atul Sharma <atulsharma406@gmail.com>
2020-09-15 23:35:43 +0530

    Add tree_preorder.cc

--- epi_judge_cpp/tree_preorder.cc
+++ epi_judge_cpp/tree_preorder.cc
@@ -4,9 +4,30 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+auto preorder_traversal_impl(std::unique_ptr<BinaryTreeNode<int>> const & tree)
+    -> std::vector<int>
+{
+    auto         result = std::vector<int>{};
+    auto         s      = std::stack<BinaryTreeNode<int> const *>{};
+    s.emplace(tree.get());
+
+    while(!s.empty())
+    {
+        auto curr = s.top();
+        s.pop();
+        if(curr)
+        {
+            result.emplace_back(curr->data);
+            s.emplace(curr->right.get());
+            s.emplace(curr->left.get());
+        }
+        
+    }
+    return result;
+}
+
 vector<int> PreorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+  return preorder_traversal_impl(tree);
 }
 
 int main(int argc, char* argv[]) {
