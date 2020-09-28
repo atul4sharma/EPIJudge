5bbf0c332c61ade6e683bfeb593080196ae9f90e
Atul Sharma <atulsharma406@gmail.com>
2020-09-28 23:04:59 +0530

    Add sunset view example

--- epi_judge_cpp/sunset_view.cc
+++ epi_judge_cpp/sunset_view.cc
@@ -3,11 +3,52 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
+struct building
+{
+    int const height;
+    int const index;
+
+    building(int h, int i)
+        : height(h)
+        , index(i)
+    {}
+};
+
+auto operator <= (building const & obj, int x)
+    -> bool
+{
+    return obj.height <= x;
+}
+
+vector<int> examine_building_with_sunset(
+    vector<int>::const_iterator sequence_begin,
+    const vector<int>::const_iterator& sequence_end)
+{
+    auto can_view_sunset = std::stack<building>{};
+    auto counter = 0;
+    while(sequence_begin != sequence_end)
+    {
+        auto const val = *sequence_begin;
+        while( (!can_view_sunset.empty()) && (can_view_sunset.top() <= val) )
+            can_view_sunset.pop();
+        can_view_sunset.emplace(val, counter);
+        std::advance(sequence_begin, 1); ++counter;
+    }
+
+    auto result = std::vector<int>{};
+    while(!can_view_sunset.empty())
+    {
+        result.emplace_back(can_view_sunset.top().index);
+        can_view_sunset.pop();
+    }
+    return result;
+}
+
 vector<int> ExamineBuildingsWithSunset(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end) {
-  // TODO - you fill in here.
-  return {};
+  return examine_building_with_sunset(sequence_begin, sequence_end);
 }
 vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int>& sequence) {
   return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
