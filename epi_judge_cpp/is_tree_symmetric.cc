46ba53567ec8a6a6b838cb6408464b86478d813e
Atul Sharma <atulsharma406@gmail.com>
2020-10-26 21:52:02 +0530

    Add is_tree_symmetric

--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,9 +1,27 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
+auto is_symmetric(std::unique_ptr<BinaryTreeNode<int>> const & left_subtree
+                 ,std::unique_ptr<BinaryTreeNode<int>> const & right_subtree)
+    -> bool
+{
+    if(left_subtree == nullptr && right_subtree == nullptr)
+    {
+        return true;
+    }
+    else if(left_subtree != nullptr && right_subtree != nullptr)
+    {
+        return left_subtree->data == right_subtree->data
+               &&
+               is_symmetric(left_subtree->left, right_subtree->right)
+               &&
+               is_symmetric(left_subtree->right, right_subtree->left);
+    }
+    return false;
+}
+
 bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+  return tree == nullptr || is_symmetric(tree->left, tree->right);
 }
 
 int main(int argc, char* argv[]) {
